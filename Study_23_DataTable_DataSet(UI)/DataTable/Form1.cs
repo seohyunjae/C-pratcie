37fc6327a853c40950d6b2923f4b44628a75a630
hyunjae <Shj229@naver.com>
2024-09-28 19:51:42 +0900

    9월 28일

--- Study_23_DataTable_DataSet(UI)/DataTable/Form1.cs
+++ Study_23_DataTable_DataSet(UI)/DataTable/Form1.cs
@@ -12,7 +12,7 @@ namespace DataTableTest
 {
     public partial class Form1 : Form
     {
-        DataSet ds =new DataSet(); // 학급들에 대한 정보를 가지고 있을 DataSet
+        DataSet ds = new DataSet(); // 학급들에 대한 정보를 가지고 있을 DataSet
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +25,33 @@ namespace DataTableTest
             dt = new DataTable(cboxRegClass.Text);
 
             DataColumn colName = new DataColumn("NAME", typeof(string));
+            DataColumn colSex = new DataColumn("SEX", typeof(string));
+            DataColumn colRef = new DataColumn("REF", typeof(string));
 
+            dt.Columns.Add(colName);
+            dt.Columns.Add(colSex);
+            dt.Columns.Add(colRef);
+
+            //Row 자료를등록
+            DataRow row = dt.NewRow();
+
+            row["NAME"] = tboxRegName.Text;
+
+            if (rdoRegSexMale.Checked)
+            {
+                row["SEX"] = "남자";
+            }
+            else
+            {
+                row["SEX"] = "여자";
+            }
+
+            row["REF"] = tboxRegRef.Text;
+
+            //생성된 Row를 Table에 등록
+            dt.Rows.Add(row);
+
+            ds.Tables.Add(dt);
         }
 
         private void btnViewDataDel_Click(object sender, EventArgs e)
