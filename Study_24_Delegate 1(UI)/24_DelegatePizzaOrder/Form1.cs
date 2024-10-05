811a049febe07434e40f5bdd80affc92fee936e4
hyunjae <Shj229@naver.com>
2024-10-05 22:59:14 +0900

    20241005

--- Study_24_Delegate 1(UI)/24_DelegatePizzaOrder/Form1.cs
+++ Study_24_Delegate 1(UI)/24_DelegatePizzaOrder/Form1.cs
@@ -21,6 +21,7 @@ namespace _24_DelegatePizzaOrder
         public delegate int delFun<T>(T i);
 
         public delegate string drug(int item);
+        public delegate string rest(int think)
         public delegate object delFunc(object i);  // var , object
 
         int _iTotalPrice = 0;
