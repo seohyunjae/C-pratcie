005600748b42de8dcf0c1475e4c4ff43810871dc
hyunjae <Shj229@naver.com>
2024-10-22 23:13:43 +0900

    10월 22일

--- Youtube/obj/Debug/net6.0/youtube.AssemblyInfo.cs
+++ Youtube/obj/Debug/net6.0/youtube.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("youtube")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Youtube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("youtube")]
-[assembly: System.Reflection.AssemblyTitleAttribute("youtube")]
+[assembly: System.Reflection.AssemblyProductAttribute("Youtube")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Youtube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
